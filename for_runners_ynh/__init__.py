179765f1174b7227c9e24f7965a028117c5f4331
Jens Diemer <git@jensdiemer.de>
2023-11-09 20:37:13 +0100

    Apply manageprojects updates
    https://github.com/jedie/cookiecutter_templates/#base-django-yunohost-app migrated from Poetry to
    pip-tools and to manifest v2
    
    Apply these changes here.

---
+++ for_runners_ynh/__init__.py
@@ -0,0 +1,7 @@
+"""
+    for_runners_ynh
+    YunoHost app package for https://github.com/jedie/django-for-runners
+"""
+
+__version__ = '0.17.4+ynh1'
+__author__ = 'Jens Diemer <git@jensdiemer.de>'
