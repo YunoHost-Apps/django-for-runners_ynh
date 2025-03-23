58de3b91b013ba25310c8d4860f307ded50a81fb
Jens Diemer <git@jensdiemer.de>
2025-03-23 11:45:40 +0100

    Migrate "pip-tools" -> "uv"

--- for_runners_ynh/__init__.py
+++ for_runners_ynh/__init__.py
@@ -1,8 +1,8 @@
 """
-    for_runners_ynh
-    YunoHost app package for https://github.com/jedie/django-for-runners
+for_runners_ynh
+YunoHost app package for https://github.com/jedie/django-for-runners
 """
 
 # See https://packaging.python.org/en/latest/specifications/version-specifiers/
-__version__ = '0.20.0+ynh2'
+__version__ = '0.20.0+ynh3'
 __author__ = 'Jens Diemer <git@jensdiemer.de>'
